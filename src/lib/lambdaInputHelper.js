63e026ca05772fdc4cbe871aa5e0060e0e7c3739
sparktwinny <spark@twinny.co.kr>
2021-05-12 16:52:54 +0900

    added float

--- src/lib/lambdaInputHelper.js
+++ src/lib/lambdaInputHelper.js
@@ -206,6 +206,9 @@ function checkInput(inputObject, apiSpec) {
   }
   return ({ "passed": true });
 }
+function isFloat(n) {
+  return Number(n) === n && n % 1 !== 0;
+}
 function iterate(apiSpec, inputObject, stack = "") {
 
   for (var property in apiSpec) {
@@ -220,6 +223,27 @@ function iterate(apiSpec, inputObject, stack = "") {
         if (val.type === "Integer") {
           if (isNaN(inputObject[property])) {
             return { "result": "invalid_type_of_parameter", "reason": "invalid_type_of_parameter,expected integer", "stack": stack + '.' + property };
+          }
+          if (inputObject[property].length > 0) {
+            const valueToInspect = parseInt(inputObject[property]);
+            if (val.min) {
+              if (val.min > valueToInspect) {
+                return { "result": "parameter_value_too_small", "reason": `parameter less than min, expected greater than ${val.min}`, "stack": stack + '.' + property };
+              }
+            }
+            if (val.max) {
+              if (val.max < valueToInspect) {
+                return { "result": "parameter_value_too_big", "reason": `parameter greater than max, expected less than ${val.max}`, "stack": stack + '.' + property };
+              }
+            }
+          }
+        }
+        if (val.type === "Float") {
+          if (isNaN(inputObject[property])) {
+            return { "result": "invalid_type_of_parameter", "reason": "invalid_type_of_parameter,expected float", "stack": stack + '.' + property };
+          }
+          if (!isFloat(inputObject[property])) {
+            return { "result": "invalid_type_of_parameter", "reason": "invalid_type_of_parameter,expected float", "stack": stack + '.' + property };
           }
           if (inputObject[property].length > 0) {
             const valueToInspect = parseInt(inputObject[property]);
