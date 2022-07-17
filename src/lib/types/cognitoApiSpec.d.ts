6fac4f703a9063df6d53ab6bf77dfbd195614a92
Sehyeon Heo <sehyeon.heo@reconlabs.kr>
2022-07-17 22:54:11 +0900

    CognitoTriggerApiSpec 수정

--- src/lib/types/cognitoApiSpec.d.ts
+++ src/lib/types/cognitoApiSpec.d.ts
@@ -1,15 +1,10 @@
-import type { PostConfirmationTriggerEvent, PreSignUpTriggerEvent } from 'aws-lambda';
-import { BaseTriggerEvent } from 'aws-lambda/trigger/cognito-user-pool-trigger/_common';
+import type { BaseCustomMessageTriggerEvent } from 'aws-lambda';
 import { ApiSpec } from './baseApiSpec';
 
-export type CognitoApiSpec = ApiSpec & {
+export type CognitoTriggerApiSpec = ApiSpec & {
   type: 'cognito';
   pool?: string; // pool을 이 프로젝트에서 생성할 때
   poolName?: string; // 기존에 생성한 pool을 사용할 때
-  trigger: string;
-};
-
-export type CognitoTriggerApiSpec<T extends BaseTriggerEvent<K>, K extends string> = CognitoApiSpec & {
-  parameters: T['request'];
-  responses: T['response'];
+  parameters: BaseCustomMessageTriggerEvent<''>['request'];
+  responses: BaseCustomMessageTriggerEvent<''>['response'];
 };
