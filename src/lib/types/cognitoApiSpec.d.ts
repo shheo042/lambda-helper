ca36a1db7b35b215347bd4b6e23d7999bc76e524
Sehyeon Heo <sehyeon.heo@reconlabs.kr>
2022-07-21 20:08:11 +0900

    poolName 대신 poolNameRef 사용하게 수정

--- src/lib/types/cognitoApiSpec.d.ts
+++ src/lib/types/cognitoApiSpec.d.ts
@@ -3,7 +3,7 @@ import { ApiSpec } from './baseApiSpec';
 
 export type CognitoTriggerApiSpec = ApiSpec & {
   type: 'cognito';
-  poolName: string;
+  poolNameRef: string;
   trigger: string;
   parameters: any;
   responses: any;
