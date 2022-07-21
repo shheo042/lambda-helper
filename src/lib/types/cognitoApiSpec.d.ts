8a385319fc36da1fb0358cdc6124e21c5a81f630
Sehyeon Heo <sehyeon.heo@reconlabs.kr>
2022-07-21 18:13:20 +0900

    cognito spec 수정

--- src/lib/types/cognitoApiSpec.d.ts
+++ src/lib/types/cognitoApiSpec.d.ts
@@ -3,8 +3,8 @@ import { ApiSpec } from './baseApiSpec';
 
 export type CognitoTriggerApiSpec = ApiSpec & {
   type: 'cognito';
-  pool?: string; // pool을 이 프로젝트에서 생성할 때
-  poolName?: string; // 기존에 생성한 pool을 사용할 때
-  parameters: BaseCustomMessageTriggerEvent<''>['request'];
-  responses: BaseCustomMessageTriggerEvent<''>['response'];
+  poolName: string;
+  trigger: string;
+  parameters: any;
+  responses: any;
 };
